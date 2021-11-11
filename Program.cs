31aa2b3f7baa793d75b895f5348cee2942fa88e9
iclalneslihan <94036441+iclalneslihan@users.noreply.github.com>
2021-11-11 11:18:15 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {   
+
+            // Bilinçsiz Tür Dönüşümü
+            // Küçük tür büyük türe otomatik olarak dönüştürülür.
+
+            byte byteSayi = 58;
+            int intSayi = byteSayi;
+
+            short shortSayi = 34;
+            long longSayi = shortSayi;
+
+            float floatSayi = 29.45f;
+            double doubleSayi = floatSayi;
+
+            Console.WriteLine("Sorunsuz Çalıştı.");
+
+
+        }
+    }
+}
